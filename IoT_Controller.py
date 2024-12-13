946d71cdc41dd0e0fd9faaf27599dc6914600203
simongoyette <148011730+simongoyette@users.noreply.github.com>
2024-12-13 16:11:35 -0500

    Update IoT_Controller.py

--- IoT_Controller.py
+++ IoT_Controller.py
@@ -1,21 +1,23 @@
+  GNU nano 7.2                                                                                                   IoT_Controller.py                                                                                                            
 import paho.mqtt.client as mqtt
+import json
+import time
 
 class IoT_Controller:
     client = None
-    #TODO: support for collections of conditions leading to single outputs
-    rules = [
-                {
-                        "condition":{"topic":"house/temp", "comparison":">", "value":30},
-                        "action":{"message":"It's too hot,turn on the AC", "topic":"room/AC","value":"On"}
-                },
-                {
-                        "condition":{"topic":"house/temp", "comparison":"<", "value":20},
-                        "action":{"message":"It's too cold,turn on the heat", "topic":"room/heat","value":"On"}
-                }
-        ]
-
+    #JSON : Javascript Object Notation is the format used for the rules below
+    #[] lists of items go between []
+    #{} lists of key.value pairs go between {} (dictionaries)
+    rules = []
+    mqtt_data = {}
+    #to remember the messages we sent out
+    message_log = []
 
     def configure():
+        filename = 'rules_LIA.json'
+        with open(filename,'r') as file:
+            IoT_Controller.rules = json.load(file)
+
         IoT_Controller.client = mqtt.Client()
         #pass the reference to the callback function to handle incoming messages
         IoT_Controller.client.on_message = IoT_Controller.on_message
@@ -24,6 +26,7 @@ class IoT_Controller:
         IoT_Controller.client.subscribe("#")
 
     def on_message(client, userdata, message):
+        print(message)
         #this is where we handle the messages
         #using try..except (exception handling)
         try:
@@ -32,36 +35,66 @@ class IoT_Controller:
             print("String")
             value = message.payload.decode("utf-8")
         topic = message.topic
+
+        #discriminate messages that i sent vs messages that i did not send
+        # if IoT_Controller.message_log contains an entry
+        for entry in IoT_Controller.message_log:
+            if entry["time"] < time.time() - 5:
+                #delete old message_log entries
+                IoT_Controller.message_log.remove(entry)
+            elif entry["topic"] == topic and entry["value"] == value:
+                return
+
+        #record the received data in our dictionary, replacing any older value for the same topic
+        IoT_Controller.mqtt_data[topic] = value
+
         #the only action is the printout
-        print(topic, value)
+        # print(topic, value)
 
         #based on the value(s) on the topic(s) received trigger an action
 
-        #loop through the rules
+       #loop through the rules
 #                {
 #                        "condition":{"topic":"house/temp", "comparison":">", "value":30},
 #                        "action":{"message":"It's too hot,turn on the AC", "topic":"room/AC","value","On"}
 #                },
 
         for rule in IoT_Controller.rules:
-            condition = rule["condition"]
-            action = rule["action"]
-            if topic == condition["topic"] and IoT_Controller.condition_met(value,condition["comparison"],condition["value"]):
+            conditions = rule["conditions"] #changed from condition
+            conditions_met = True
+            for condition in conditions:
+                #use the topic from the condition to access the value in the mqtt_data dictionary
+                topic = condition["topic"]
+                try:
+                    value = IoT_Controller.mqtt_data[topic] #not going to work if there is no value for the key provided
+                    condition_met = IoT_Controller.condition_met(value,condition["comparison"],condition["value"])
+                except KeyError:
+                    value = None
+                    condition_met = False
+                conditions_met = conditions_met and condition_met
+            print(conditions_met)
+            if conditions_met:
                 #action
+                action = rule["action"]
                 print(action["message"])
                 IoT_Controller.client.publish(action["topic"],action["value"])
+                #record that we sent that message
+                entry = {"time":time.time(), "topic":action["topic"], "value":action["value"]}
+                IoT_Controller.message_log.append(entry) #add the item to the end of the list
 
     def condition_met(value,comp_operator,comp_value):
+        print(f"condition_met({value},{comp_operator},{comp_value})")
+
         if comp_operator == ">":
             return value > comp_value
         if comp_operator == ">=":
-            return value > comp_value
+            return value >= comp_value
         if comp_operator == "<":
-            return value > comp_value
+            return value < comp_value
         if comp_operator == "<=":
-            return value > comp_value
+            return value <= comp_value
         if comp_operator == "==":
-            return value > comp_value
+            return value == comp_value
 
     def run():
         IoT_Controller.client.loop_forever()
